63d0ee9d277adf236f563d10027497c2e6cd8e3f
Nick <nicolas.gonzalez.araneda@ciisa.cl>
2018-11-05 14:33:21 -0400

    Menu navegación Clentes Funcinando!!

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente'
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Nicolas', apellido: 'González',email: 'nicoimpresos@gmail.com', createAt: '2018-11-01' },
+    {id: 2, nombre: 'Monserrat', apellido: 'Araneda',email: 'otro1@gmail.com', createAt: '2018-11-01' },
+    {id: 3, nombre: 'Carolina', apellido: 'Ovalle',email: 'otro2@gmail.com', createAt: '2018-11-01' },
+    {id: 4, nombre: 'Leo', apellido: 'Carvajal',email: 'otro3@gmail.com', createAt: '2018-11-01' },
+    {id: 5, nombre: 'Edu', apellido: 'Zuñiga',email: 'otro4@gmail.com', createAt: '2018-11-01' },
+    {id: 6, nombre: 'Laura', apellido: 'Salazar',email: 'otro5@gmail.com', createAt: '2018-11-01' },
+    {id: 7, nombre: 'Julián', apellido: 'Grifindoor',email: 'otro6@gmail.com', createAt: '2018-11-01' },
+    {id: 8, nombre: 'Calisto', apellido: 'Juli',email: 'otro7@gmail.com', createAt: '2018-11-01' }
+  ];
